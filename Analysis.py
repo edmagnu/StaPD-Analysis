11d68624c801fcd7643b1e215086d7ab96bfe50c
Eric Magnuson <edmagnu@gmail.com>
2017-12-19 20:12:36 -0500

    Run plots and fits on each data file.
    Each delay scan now has a partner .pdf including data, running average,
    and the model fit.
    "moddata.txt" adds the fit parameters and model values to the
    measurements.
    "fits.txt" has a single observation per file, including all the metadata
    and fit parameters, but excluding any inidividual data points.

--- Analysis.py
+++ Analysis.py
@@ -8,10 +8,11 @@ Created on Sun Oct  8 11:24:19 2017
 # Static Phase Dependence (StaPD) Experiment Data Analysis
 # Eric Magnuson, University of Virginia, VA
 
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import scipy.optimize
 import os
+import pandas as pd
 
 
 def dlist_gen(path):
@@ -38,7 +39,7 @@ def folderlist_gen(fname):
     files = []  # list of files in all folders
     for folder in folders:
         files = files + dlist_gen(folder + "\\")  # dlist_gen by folder
-    return files 
+    return files
 
 
 def read_metadata(fname):
@@ -125,23 +126,128 @@ def model_func(x, y0, a, phi):
     return y0 + a*np.sin(2*np.pi*x + phi)
 
 
+def model_p0(x, y):
+    """Guesses reasonable starting parameters p0 to pass to model_func().
+    x and y are pandas.Series
+    Returns p0, array of [y0, a, phi]"""
+    # y0 is the mean
+    y0 = y.mean()
+    # phi from averaged maximum and minimum
+    yroll = y.rolling(window=9, center=True).mean()
+    imax = yroll.idxmax()
+    imin = yroll.idxmin()
+    phi = ((x[imax] % 1) + ((x[imin]-0.5) % 1)) / 2
+    phi = ((phi-0.25) % 1)*np.pi
+    # a is max and min
+    mx = yroll.max()
+    mn = yroll.min()
+    a = (mx-mn)/2
+    return [y0, a, phi]
+
+
+def plot_dscan(data, filename, save=False, close=False):
+    """Given a DataFrame with multiple delay scans, select one by the
+    filename. Plot the data, running average, and a model fit. Assumes data
+    comes with "wavelenth", "nsignal", "y0", "a", "phi", and "model".
+    Returns None"""
+    # pick out one file
+    data = data[data["Filename"] == filename]
+    data = data.sort_values(by=["wavelengths"])
+    # produce a rolling average
+    rave = data[["wavelengths", "nsignal"]].rolling(window=9, center=True)
+    rave = rave.mean()
+    rave = rave[~np.isnan(rave["nsignal"])]
+    # extract model p0 [y0, a, phi]
+    p0 = np.array([float(data["y0"][[0]]), float(data["a"][[0]]),
+                   float(data["phi"][[0]])])
+    # plot the data, model, and rolling average
+    axes = data.plot(x="wavelengths", y="nsignal", kind="scatter",
+                     label="data")
+    data.plot(x="wavelengths", y="model", kind="line", color="black",
+              label="fit", ax=axes)
+    rave.plot(x="wavelengths", y="nsignal", kind="line", color="red",
+              label="rolling ave.", ax=axes)
+    # stamp with useful information
+    fcent = ((data["DL-Pro"].unique()[0] + data["DL-100"].unique()[0])/2
+             - 365869.6)
+    static = data["Static"].unique()[0]*0.1*0.72
+    stamp = "y0 = {:2.3f}\na = {:2.3f}\nphi = {:2.2f}".format(p0[0], p0[1],
+                                                              p0[2]/(2*np.pi))
+    stamp = stamp + "\nf_cent = {:2.0f} GHz".format(fcent)
+    stamp = stamp + "\nF_st = {:2.2f} mV/cm".format(static)
+    plt.text(0.05, 0.05,
+             stamp,
+             transform=axes.transAxes, bbox=dict(facecolor="white"))
+    axes.set_xlabel("Delay (MW periods)")
+    axes.set_ylabel("Rydberg Signal")
+    axes.set_title(filename)
+    # plt.grid(True)
+    plt.tight_layout()
+    if save is True:
+        fname = "Modified Data\\" + filename.split(".")[0] + ".pdf"
+        print(fname)
+        plt.savefig(fname)
+    if close is True:
+        plt.close()
+
+
+def build_fits(data):
+    """Given a dataframe with "wavelengths" and "nsignal", fits to the
+    model_func(), and adds fit parameters y0, a and phi to the DataFrame.
+    Then, add a column to the data frame of the fitted values.
+    Save the new data frame to "moddata.txt"
+    Returns data DataFrame with "y0", "a", "phi" and "model" keys added.
+    """
+    # add columns of NaN for each new key.
+    dl = data.shape[0]
+    data["y0"] = np.ones(dl)*np.NaN
+    data["a"] = np.ones(dl)*np.NaN
+    data["phi"] = np.ones(dl)*np.NaN
+    data["model"] = np.ones(dl)*np.NaN
+    # build DataFrame for metadata and fit params, not each point.
+    fits = pd.DataFrame()
+    # get a list of unique filenames
+    filelist = data["Filename"].unique()
+    imax = len(filelist)
+    for i, filename in enumerate(filelist):
+        print(i+1, " / ", imax)  # progress
+        mask = data["Filename"] == filename  # mask all but one file
+        # data is often triangle in delay, sort it
+        dsort = data[mask].sort_values(by=["wavelengths"])
+        # fit to sinusoidal model_func()
+        p0 = model_p0(dsort["wavelengths"], dsort["nsignal"])  # bestt guess
+        # get fit parameters
+        popt, pcov = scipy.optimize.curve_fit(
+                model_func, dsort["wavelengths"], dsort["nsignal"], p0)
+        # fill in fit parameters and model nsignal
+        data.loc[mask, "y0"] = popt[0]
+        data.loc[mask, "a"] = popt[1]
+        data.loc[mask, "phi"] = popt[2]
+        # use original data, not the sorted data
+        data.loc[mask, "model"] = model_func(data[mask]["wavelengths"], *p0)
+        # build fits DataFrame
+        keylist = ["Filename", "DL-Pro", "DL-100", "Static", "MWOn", "MWf",
+                   "Attn"]
+        fit = data.iloc[0][keylist]
+        fit["y0"] = p0[0]
+        fit["a"] = p0[1]
+        fit["phi"] = p0[2]
+        fits = fits.append(fit)
+    # force fits key order
+    fits = fits[keylist + ["y0", "a", "phi"]]
+    # export and return result]
+    data.to_csv("moddata.txt", sep="\t")
+    fits.to_csv("fits.txt", sep="\t")
+    return data, fits
+
+
 # main program starts here
-data = pd.read_csv("rawdata.txt", sep="\t", index_col=0)  # read in all data
-# plot an individual file
-filelist = data["Filename"].unique()  # get list of files
-mask = data["Filename"] == filelist[0]  # mask all but one file to work on
-dtemp = data[mask].sort_values(by=["wavelengths"])
-rave = dtemp[["wavelengths", "nsignal"]].rolling(window=9, center=True)
-rave = rave.mean()
-rave = rave[~np.isnan(rave["nsignal"])]
-axes = dtemp.plot(x="wavelengths", y="nsignal", kind="scatter")
-model = pd.DataFrame()
-model["x"] = dtemp["wavelengths"].sort_values()
-p0 = [0.047, 0.015, 0.4*np.pi]
-popt, pcov = scipy.optimize.curve_fit(
-        model_func, dtemp["wavelengths"], dtemp["nsignal"], p0)
-print("y0 = ", popt[0], "a = ", popt[1], "phi = ", popt[2])
-model["y"] = model_func(model["x"], *popt)
-model.plot(x="x", y="y", kind="line", color="black", ax=axes)
-rave.plot(x="wavelengths", y="nsignal", kind="line", color="red", ax=axes)
-# print(~np.isnan(rave["nsignal"]))
+def main():
+    """Read in raw data Add fit parameters and model values and save."""
+    # read in all data with fit info
+    data = pd.read_csv("rawdata.txt", sep="\t", index_col=0)
+    data, fits = build_fits(data)
+    return data, fits
+
+
+data, fits = main()
